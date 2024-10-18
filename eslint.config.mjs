680f0bc991e83485a20c98b8a701ccf3362ff94a
Suzy Kim <skme1103@naver.com>
2024-10-18 17:36:54 +0900

    Fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,18 @@ import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      env: {
+        browser: true, // 브라우저 환경
+        es2021: true,  // ECMAScript 2021
+      },
+      parser: tseslint.parsers.TypeScriptParser, // TypeScript 파서 설정 (필요한 경우)
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
